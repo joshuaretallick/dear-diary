905c449c8363b3594fa8ced9b0d407d7678ced21
Joshua Retallick <josh@old-empire.co.uk>
2021-03-09 09:55:59 +0000

    Added has_many cross associations between articles and categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_110638) do
+ActiveRecord::Schema.define(version: 2021_03_09_094344) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
